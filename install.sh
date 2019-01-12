d5b4969ed2b923835a0de50bf6bf2187c6c91649
Ahren-Li <lili@vamrs.com>
2019-01-12 16:53:16 +0800

    add sudo
    Signed-off-by: Ahren-Li <lili@vamrs.com>

--- install.sh
+++ install.sh
@@ -4,25 +4,31 @@ if [[ ! $UBUNTU ]]; then
 fi
 
 if [ ! -z "$CORE_SW_LIST" ];then
-	apt-get update
+	sudo apt-get update
 	for core in $CORE_SW_LIST
 	do
 		INFO_INSTALL "[CORE] $core"
-		apt-get install -y $core
+		sudo apt-get install -y $core
 	done
 fi
+
+for ppa_key in $PPA_KEY_LIST
+	do
+		INFO_PPA $ppa_key
+		wget -qO - $ppa_key | sudo apt-key add -
+	done
 
 if [ ! -z "$PPA_LIST" ];then
 	for ppa in $PPA_LIST
 	do
 		INFO_PPA $ppa
-		apt-add-repository -y $ppa
+		sudo apt-add-repository -y $ppa
 	done
-	apt-get update
+	sudo apt-get update
 fi
 
 for sw in $SW_LIST
 do
 	INFO_INSTALL $sw
-	apt-get install -y $sw
+	sudo apt-get install -y $sw
 done
